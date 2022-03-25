ead6a99d144baf607be6bf7186d1990975addb4d
Valery <valera.koltirin@yandex.ru>
2022-03-25 13:24:48 +0300

    Compleate HW2 core 5

---
+++ First API/First API/Controllers/MetricControllers/DotNetMetricsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace First_API.Controllers
+{
+    [Route("api/metrics/dotnet")]
+    [ApiController]
+    public class DotNetMetricsController : ControllerBase
+    {
+        [HttpGet("agent/{agentId}/from/{fromTime}/to/{toTime}")]
+        public IActionResult GetMetricsFromAgent
+            (
+            [FromRoute] int agentId,
+            [FromRoute] TimeSpan fromTime,
+            [FromRoute] TimeSpan toTime
+            )
+        {
+            return Ok();
+        }
+
+        [HttpGet("cluster/from/{fromTime}/to/{toTime}")]
+        public IActionResult GetMetricsFromAllCluster
+            (
+            [FromRoute] TimeSpan fromTime,
+            [FromRoute] TimeSpan toTime
+            )
+        {
+            return Ok();
+        }
+    }
+}
